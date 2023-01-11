096fff1a1d9455014842809550d21b72e3e838e3
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-11 08:54:19 -0700

    This is just to show a basic if else statement along with some user input!

--- Hello World/Program.vb
+++ Hello World/Program.vb
@@ -2,6 +2,16 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Type a word plz")
+        Dim message As String = ""
+        message = Console.ReadLine()
+        Console.WriteLine($"You Said: {message}?")
+        Console.WriteLine("Is that right?")
+        Dim UsrConfirm = Console.ReadLine()
+        If (UsrConfirm = "yes") Then
+            Console.WriteLine("Awesome lol!")
+        Else
+            Console.WriteLine("Too Bad lol!")
+        End If
     End Sub
 End Module
